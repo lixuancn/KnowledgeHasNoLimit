0abf12ad3ef473ec31e024cd55da4b77399b891a
lixuancn <lixuan868686@163.com>
2018-06-10 01:07:19 +0800

    go实现Memcache协议

--- Memcache-go/main.go
+++ Memcache-go/main.go
@@ -1,82 +1,15 @@
 package main
 
 import (
-	"net"
-	"fmt"
-	"bufio"
-	"strings"
+	"Memcache-go/server"
 )
 
+const TCP_NETWORK = "tcp"
+const TCP_HOST = "127.0.0.1"
+const TCP_PORT = "19910"
+const TCP_PROTOCOL = "memcache"
 
 func main(){
-	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:19910")
-	if err != nil{
-		fmt.Println(err)
-		return
-	}
-	tcpListener, err := net.ListenTCP("tcp", tcpAddr)
-	if err != nil{
-		fmt.Println(err)
-		return
-	}
-	defer tcpListener.Close()
-	fmt.Println("tcp开启")
-	for {
-		clientConn, err := tcpListener.AcceptTCP()
-		if err != nil{
-			fmt.Println(err)
-			continue
-		}
-		fmt.Println("新连接: " + clientConn.RemoteAddr().String())
-		go tcpPipe(clientConn)
-	}
-}
-
-func tcpPipe(conn *net.TCPConn){
-	ipStr := conn.RemoteAddr().String()
-	defer func(){
-		fmt.Println("断开连接：" + ipStr)
-		conn.Close()
-	}()
-	reader := bufio.NewReader(conn)
-	message := ""
-	for{
-		msg, err := reader.ReadString('\n')
-		if err != nil{
-			fmt.Println("错误: " + err.Error())
-			return
-		}
-		message += msg
-		result := parse(message)
-		if result{
-			conn.Write([]byte("STORED\r\n"))
-			message = ""
-		}
-	}
-}
-
-func parse(message string) bool{
-	fmt.Println(len(message))
-	if message[len(message)-1] != '\n'{
-		fmt.Println("协议错误1")
-		return false
-	}
-	if message[len(message)-2] != '\r'{
-		fmt.Println("协议错误2")
-		return false
-	}
-	headList := strings.Split(message, "\r\n")
-	fmt.Println( len(headList))
-	if len(headList) != 3{
-		return false
-	}
-	return true
-	fmt.Println("消息：" + message)
-	headList = strings.Split(message, " ")
-	if headList[0] != "set" || len(headList) != 5 || len(headList[4]) <= 0{
-		fmt.Println("协议错误3")
-		return false
-	}
-
-	return true
+	tcpServer := server.NewTcpServer(TCP_NETWORK, TCP_HOST, TCP_PORT, TCP_PROTOCOL)
+	tcpServer.Run()
 }
---
+++ Memcache-go/server/tcp.go
@@ -0,0 +1,67 @@
+package server
+
+import (
+	"net"
+	"log"
+	"fmt"
+)
+
+type TcpServer struct{
+	network string
+	host string
+	port string
+	protocol Protocol
+	tcpListener *net.TCPListener
+	clientConnList []net.Conn
+}
+
+func NewTcpServer(network, host, port, protocol string)*TcpServer{
+	pro, err := GetProtocol(protocol)
+	if err != nil{
+		log.Fatalf("应用层协议错误")
+	}
+	return &TcpServer{
+		network: network,
+		host: host,
+		port: port,
+		protocol: pro,
+	}
+}
+
+func (tcp *TcpServer) Run (){
+	fmt.Println("TCP服务启动中...")
+	tcpAddr, err := net.ResolveTCPAddr(tcp.network, tcp.host + ":" + tcp.port)
+	if err != nil{
+		log.Fatalf("构造TCP失败")
+	}
+	tcpListener, err := net.ListenTCP(tcp.network, tcpAddr)
+	if err != nil{
+		log.Fatalf("TCP监听失败")
+	}
+	tcp.tcpListener = tcpListener
+	defer tcpListener.Close()
+	log.Println("TCP服务启动完成")
+	tcp.loop()
+}
+
+func (tcp *TcpServer)loop(){
+	log.Println("TCP服务等待链接")
+	for {
+		clientConn, err := tcp.tcpListener.AcceptTCP()
+		if err != nil{
+			log.Println("接受连接失败" + err.Error())
+			continue
+		}
+		log.Println("接收新连接: " + clientConn.RemoteAddr().String())
+		tcp.clientConnList = append(tcp.clientConnList, clientConn)
+		go tcp.Handle(clientConn)
+	}
+}
+
+func (tcp *TcpServer)Handle(clientConn net.Conn){
+	defer func(){
+		fmt.Println("断开连接：" + clientConn.RemoteAddr().String())
+		clientConn.Close()
+	}()
+	tcp.protocol.Handle(clientConn)
+}
